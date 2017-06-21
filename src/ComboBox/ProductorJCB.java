0e81becd85df46f3469e887bed7794a2f234aaf7
josekilljoy <josekilljoy95@gmail.com>
2017-06-21 01:00:28 -0300

    Interfaces de producción sin boton de salir implementado

---
+++ src/ComboBox/ProductorJCB.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ComboBox;
+
+import Conexion.SQLconnection;
+import Diseño.Productor;
+import java.sql.ResultSet;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author josekilljoy95
+ */
+public class ProductorJCB {
+    private SQLconnection conn= new SQLconnection();    
+    
+    
+        public void listar_los_productores(JComboBox box){
+        DefaultComboBoxModel value;
+        int i;
+        ResultSet rs;
+        conn.connect();
+        rs=conn.getProductoresResult();
+        try{
+           
+            value = new DefaultComboBoxModel();
+            box.setModel(value);
+            
+            while(rs.next()){
+               
+             
+            //value.addElement(rs.getString(3));//new Productor(rs.getString(3),rs.getString(1),rs.getString(2)));  
+            box.addItem(rs.getString(3));
+            }
+        }catch(Exception ex){
+        }finally{
+            try{
+                conn.close();
+            }catch(Exception ex){
+                
+            }
+        }
+    }
+
+
+}
---
+++ src/ComboBox/TamboCB.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ComboBox;
+
+import Clases.Tambo;
+import Conexion.SQLconnection;
+import Diseño.Productor;
+import java.sql.ResultSet;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author josekilljoy95
+ */
+public class TamboCB {
+     private SQLconnection conn= new SQLconnection();    
+    
+    
+        public void listar_los_Tambos(JComboBox box, String cod_E){
+        DefaultComboBoxModel value;
+        int i;
+        ResultSet rs;
+        try{
+            conn.connect();
+            rs=conn.getTambosAsociados(cod_E);
+            value = new DefaultComboBoxModel();
+            box.setModel(value);
+            
+            while(rs.next()){
+               
+              //value.addElement(rs.getString(1));//new Tambo(rs.getString(3),rs.getString(2),rs.getString(1),rs.getString(4))); 
+              box.addItem(rs.getString(1));
+            }
+        }catch(Exception ex){
+        }finally{
+            try{
+                conn.close();
+            }catch(Exception ex){
+                
+            }
+        }
+}
+}
